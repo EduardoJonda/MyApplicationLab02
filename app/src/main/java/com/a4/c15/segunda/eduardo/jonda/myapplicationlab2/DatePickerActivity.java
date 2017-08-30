f7cc153afe203065da3ca66cbdaa3801c9692462
EduardoJonda <r7sok.18kn@hotmail.com>
2017-08-29 20:45:35 -0500

    Initial commit

---
+++ app/src/main/java/com/a4/c15/segunda/eduardo/jonda/myapplicationlab2/DatePickerActivity.java
@@ -0,0 +1,45 @@
+package com.a4.c15.segunda.eduardo.jonda.myapplicationlab2;
+
+import android.icu.util.Calendar;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.TextView;
+
+public class DatePickerActivity extends AppCompatActivity {
+
+    private DatePicker datePicker;
+    private TextView displayDate;
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_date_picker);
+
+        displayDate = (TextView) findViewById(R.id.display_date);
+        datePicker = (DatePicker) findViewById(R.id.datePicker);
+
+        // current date
+        int year = Calendar.getInstance().get(Calendar.YEAR);
+        int month = Calendar.getInstance().get(Calendar.MONTH);
+        int day = Calendar.getInstance().get(Calendar.DAY_OF_MONTH);
+
+        datePicker.init(year,month,day,  new DatePicker.OnDateChangedListener(){
+            @Override
+            public void onDateChanged(DatePicker view, int yy, int mm, int dd){
+                displayDate.setText("Date: " + dd + "/" + (mm+1) + "/" + yy);
+            }
+        });
+
+    }
+
+    public void pick(View view){
+        String currentDate = "Picked: " + datePicker.getDayOfMonth() + "/" + (datePicker.getMonth() + 1) + "/" + datePicker.getYear();
+        displayDate.setText(currentDate);
+    }
+
+}
---
+++ app/src/main/java/com/a4/c15/segunda/eduardo/jonda/myapplicationlab2/SpinnerActivity.java
@@ -0,0 +1,10 @@
+package com.a4.c15.segunda.eduardo.jonda.myapplicationlab2;
+
+import android.app.Activity;
+
+/**
+ * Created by Alumno on 29/8/2017.
+ */
+
+public class SpinnerActivity extends Activity {
+}
